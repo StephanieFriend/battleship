135f1c5ebe4129905fca5df9c76e187e57400f0f
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-03 16:11:25 -0700

    Write initialize and place ship methods
    Co-Authored-By: Stephanie Friend <StephanieFriend@users.noreply.github.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,7 +1,22 @@
 class Cell
-attr_reader :coordinate 
+attr_reader :coordinate,
+            :ship,
+            :empty
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+    @empty = true
   end
+
+  def empty?
+    empty
+  end
+
+  def place_ship(ship)
+    @ship = ship
+    @empty = false 
+  end
+
+
 end
