56a542d3b9acd50fc9226276935e081814c93636
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-03 16:10:12 -0700

    Write tests for creation of cell and place ship
    Co-Authored-By: Stephanie Friend <StephanieFriend@users.noreply.github.com>

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -16,4 +16,26 @@ class CellTest < Minitest::Test
 
     assert_equal "B4", cell.coordinate
   end
+
+  def test_ship_does_not_exist
+    cell = Cell.new("B4")
+
+    assert_nil cell.ship
+  end
+
+  def test_cell_defaults_as_empty
+    cell = Cell.new("B4")
+
+    assert_equal true, cell.empty?
+  end
+
+  def test_place_ship_on_cell
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+
+    assert_equal cruiser, cell.ship
+    refute cell.empty?
+  end
 end
