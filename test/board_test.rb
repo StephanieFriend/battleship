402c2f5d6cf4e59f22c5cb58e586c1da60c15f0b
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-04 16:09:15 -0700

    Write tests for valid coordinates and valid placement
    Co-Authored-By: Stephanie Friend <StephanieFriend@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -8,6 +8,8 @@ class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exits
@@ -19,4 +21,17 @@ class BoardTest < Minitest::Test
     assert_equal 16, @board.cells.count
   end
 
+  def test_that_board_can_validate_coordinates
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal false, @board.valid_coordinate?("A5")
+    assert_equal true, @board.valid_coordinate?("D4")
+    assert_equal false, @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("A22")
+  end
+
+  def test_board_can_validate_length_of_ship_placement
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+
+  end
 end
