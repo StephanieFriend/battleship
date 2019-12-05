5b4217f9df7395e5bacfd33a20fe18c20c27cca7
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-05 16:58:51 -0700

    write horizontal methods and finalized valid placement method
    Co-Authored-By: Stephanie Friend <StephanieFriend@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -26,21 +26,18 @@ attr_reader :cells
   end
 
   def valid_placement?(ship, coordinates)
-    ship.length == coordinates.length
-    # && coordinates.empty?
+    ship.length == coordinates.length && (vertical_placement?(coordinates) || horizontal_placement?(coordinates))
   end
 
   def vertical_placement?(coordinates)
-    #array of letter order
-    #bring numbers and letters method together to
-    #test if they are valid
-    #maybe see if we can test if they are also
-    #inside the array of letters with only letters method
     vertical_numbers?(coordinates) && vertical_letters?(coordinates)
   end
 
+  def horizontal_placement?(coordinates)
+    horizontal_letters?(coordinates) && horizontal_numbers?(coordinates)
+  end
+
   def vertical_numbers?(coordinates)
-    #verifying that all the numbers are the same
     numbers = coordinates.map do |coordinate|
       coordinate.chars.last
     end
@@ -48,7 +45,6 @@ attr_reader :cells
   end
 
   def vertical_letters?(coordinates)
-    #verifying that the letters are consecutive
     new_range_array = []
     letters_range = ("A".."D").to_a
     letters = coordinates.map do |coordinate|
@@ -64,7 +60,19 @@ attr_reader :cells
     letters = coordinates.map do |coordinate|
       coordinate.chars.first
     end
-    letters.uniq.length == 1 
+    letters.uniq.length == 1
+  end
+
+  def horizontal_numbers?(coordinates)
+    new_range_array = []
+    numbers_range = ("1".."4").to_a
+    numbers = coordinates.map do |coordinate|
+      coordinate.chars.last
+    end
+    numbers_range.each_cons(coordinates.length) do |number_range|
+      new_range_array << number_range
+    end
+    new_range_array.include?(numbers)
   end
 
 end
