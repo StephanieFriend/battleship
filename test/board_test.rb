df1c17f01e0c3aa64eed215e2815979e6f8488aa
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-04 17:19:37 -0700

    Wrote test for consecutive coordinates valid placement
    Co-Authored-By: Stephanie Friend <StephanieFriend@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -32,6 +32,14 @@ class BoardTest < Minitest::Test
   def test_board_can_validate_length_of_ship_placement
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
 
+  def test_coordinates_are_consecutive
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["A1", "B1", "C1"])
+    assert_equal true, @board.valid_placement?(@submarine, ["D2", "D3"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
   end
 end
