1fbe73bec492242acc7f9c8943593419f928deb3
David Holtkamp <david.hholtkamp@gmail.com>
2019-12-10 13:56:06 -0700

    Add computer setup method to file
    Co-Authored-By: Stephanie Friend <StephanieFriend@users.noreply.github.com>

--- lib/runner.rb
+++ lib/runner.rb
@@ -4,7 +4,10 @@ require_relative '../lib/board'
 require_relative '../lib/game'
 
 game = Game.new
+cruiser = Ship.new("Cruiser", 3)
+submarine = Ship.new("Submarine", 2)
 
 game.opening_message
+game.computer_setup(cruiser)
+game.computer_setup(submarine)
 game.welcome
-game.player_place_cruiser
